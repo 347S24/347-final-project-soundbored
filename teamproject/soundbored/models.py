45b31f72c8a7ea64c9adb2abe6102a1bb1e03eca
Dylan Aitcheson <aitchedx@dukes.jmu.edu>
2024-04-09 16:46:02 -0400

    added validation on audio upload. now only allows for mp3 files

--- teamproject/soundbored/models.py
+++ teamproject/soundbored/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from django.core.validators import FileExtensionValidator
 
 class Audio(models.Model):
     title = models.CharField(max_length=255)
-    audio_file = models.FileField(upload_to='audios/')
+    audio_file = models.FileField(upload_to='audios/', validators=[FileExtensionValidator(allowed_extensions=['mp3'])])
     image = models.ImageField(upload_to='images/', default='images/defaultpic.jpeg')
     uploaded_at = models.DateTimeField(auto_now_add=True)
