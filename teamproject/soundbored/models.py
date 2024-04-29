1e8072dc45da8994c24de668357e54bf4554f6d5
Dylan Aitcheson <aitchedx@dukes.jmu.edu>
2024-04-28 23:42:39 -0400

    added floppy icon to sounds

--- teamproject/soundbored/models.py
+++ teamproject/soundbored/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Audio(models.Model):
     title = models.CharField(max_length=255)
     audio_file = models.FileField(upload_to='audios/', validators=[FileExtensionValidator(allowed_extensions=['mp3'])])
-    image = models.ImageField(upload_to='images/', default='images/defaultpic.jpeg')
+    image = models.ImageField(upload_to='images/', default='images/defaultpic.png')
     uploaded_at = models.DateTimeField(auto_now_add=True)
     favorites = models.ManyToManyField(User, related_name='favorite_audios')
 
