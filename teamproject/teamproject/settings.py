7c5d1de6a328416a50060f64620305361b941dc2
Dylan Aitcheson <aitchedx@dukes.jmu.edu>
2024-05-03 14:38:42 -0400

    password reset now actually working

--- teamproject/teamproject/settings.py
+++ teamproject/teamproject/settings.py
@@ -127,4 +127,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# # For testing password reset to the console
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# Real SMTP config 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'soundboredteam@gmail.com'
+EMAIL_HOST_PASSWORD = 'damw bpgh ebpz sojk'
+
